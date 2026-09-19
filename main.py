178eeffd842b3a58269323d6bfa877990f715289
vanig245 <vani.gupta2405@gmail.com>
2026-09-19 13:26:50 +0530

    feat: add authentication to bypass cloudflare

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 from playwright.sync_api import sync_playwright
-from playwright_stealth import stealth_sync
+from playwright_stealth import Stealth
 
 AUTH_FILE = "job_state.json"
 
 def authenticate_wellfound():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
+    with Stealth().use_sync(sync_playwright()) as p:
+        browser = p.chromium.launch(
+            headless=False, 
+            args=["--disable-blink-features=AutomationControlled"]
+        )
         
         if os.path.exists(AUTH_FILE):
             context = browser.new_context(storage_state=AUTH_FILE)
@@ -16,12 +19,10 @@ def authenticate_wellfound():
             print("Manual login required.")
             
         page = context.new_page()
-        stealth_sync(page)
-        
         page.goto("https://wellfound.com/jobs")
         
         if not os.path.exists(AUTH_FILE):
-            print("Please log in to Wellfound in the opened browser...")
+            print("Please log in to Wellfound in the opened browser")
             page.wait_for_url("https://wellfound.com/jobs*", timeout=120000)
             context.storage_state(path=AUTH_FILE)
             print(f"Session cookies saved to {AUTH_FILE}")
